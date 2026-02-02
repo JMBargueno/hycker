975bc1b98662c492091cf0114c3c8d2c00ab5cb6
Jose Manuel Bargueno Lopez <contact@jmbargueno.com>
2026-02-02 14:52:41 +0100

    Progress with improvements

--- scripts/mods-downloader.sh
+++ scripts/mods-downloader.sh
@@ -13,19 +13,28 @@ set -e
 SCRIPT_DIR="$(dirname "$0")"
 DL_DIR="$SCRIPT_DIR/mods_downloaders"
 
+ANY_MODS=false
+
 if [ -n "$HYCKER_MODS_ZIP_URL" ]; then
   echo "[HYCKER - DOWNLOADER ORCHESTRATOR] Detected HYCKER_MODS_ZIP_URL. Calling download-mods-from-zip-url.sh..."
   bash "$DL_DIR/download-mods-from-zip-url.sh" "$HYCKER_MODS_ZIP_URL"
+  ANY_MODS=true
 fi
 
 if [ -n "$HYCKER_MODS_GDRIVE_URL" ]; then
   echo "[HYCKER - DOWNLOADER ORCHESTRATOR] Detected HYCKER_MODS_GDRIVE_URL. Calling download-mods-gdrive.sh..."
   bash "$DL_DIR/download-mods-gdrive.sh" "$HYCKER_MODS_GDRIVE_URL"
+  ANY_MODS=true
 fi
 
 if [ -n "$HYCKER_MODS_CURSEFORGE_IDS" ]; then
   echo "[HYCKER - DOWNLOADER ORCHESTRATOR] Detected HYCKER_MODS_CURSEFORGE_IDS. Calling download-mods-curseforge.sh..."
   bash "$DL_DIR/download-mods-curseforge.sh" "$HYCKER_MODS_CURSEFORGE_IDS"
+  ANY_MODS=true
 fi
 
-echo "[HYCKER - DOWNLOADER ORCHESTRATOR] All requested mod downloads completed."
+if [ "$ANY_MODS" = false ]; then
+  echo "[HYCKER - DOWNLOADER ORCHESTRATOR] No mods detected to install."
+else
+  echo "[HYCKER - DOWNLOADER ORCHESTRATOR] All requested mod downloads completed."
+fi
